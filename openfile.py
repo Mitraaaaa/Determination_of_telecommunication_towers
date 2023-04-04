14c04a5f3005bc59511edde2bf357dceadb277aa
SmN <52829836+SamanOmidi@users.noreply.github.com>
2023-04-04 19:02:17 +0430

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -1,43 +1,21 @@
 import json
+
 def read_file_int(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
     data = []
     for line in lines:
         row = list(map(int,line.strip().split(',')))
-        data.append(row)
+        for number in row:
+            data.append(number)
     return data
-import json
-
-import json
-
-import json
-
-import json
 
 def text_to_json(file_path):
-    # Open the text file
-    with open(file_path, 'r') as file:
-        # Read the contents of the file
-        contents = file.read()
-
-    # Split the contents into lines
-    lines = contents.split('\n')
-
-    # Create a dictionary to store the data
-    data = {}
-
-    # Loop through the lines and add them to the dictionary
-    for line in lines:
-        if ':' in line:
-            key, value = line.split(':')
-            data[key.strip()] = value.strip()
-
-    # Convert the dictionary to a JSON object
-    json_data = json.dumps(data)
-
+    json_data = None
+    with open(file_path) as f:
+        json_data = json.load(f)
     # Return the JSON object
     return json_data
-# l = read_file('blocks_population.txt')
-l = text_to_json('problem_config.txt')
-print(l)
+
+block_population = read_file_int('blocks_population.txt')
+problem_config = text_to_json('problem_config.txt')
