24edb33788278e0852ed0178a9b622c1e191e57c
Mitra Omrani <83425856+Mitraaaaa@users.noreply.github.com>
2023-04-05 13:52:06 +0330

    Update divide_regions.py
    split areas into a logical number of regions given by the user

--- divide_regions.py
+++ divide_regions.py
@@ -1,6 +1,11 @@
 import math
-def divide_grid(N):
-    region_size = int(20 / math.sqrt(N))
+import numpy
+def divide_grid(N , dp_squares : list):
+    # N is the number of regions U want to split your area, * it's not the lines of division
+    N = math.sqrt(dp_squares[N])
+    print(N)
+    region_size = math.ceil(20/N)
+    print(region_size)
     regions = []
     for i in range(0, 20, region_size):
         for j in range(0, 20, region_size):
@@ -13,8 +18,25 @@ def divide_grid(N):
     return regions
 
 
-l = divide_grid()
+def find_closest_square():
+    dp_closest_squares = numpy.zeros(401)
+    for i in range(1, 21):
+        dp_closest_squares[i*i] = 1
 
+    tmp = 400
+    for i in range(400, -1, -1):
+        if dp_closest_squares[i] == 1:
+            dp_closest_squares[i] = i
+            tmp = i
+        else:
+            dp_closest_squares[i] = tmp
+    return dp_closest_squares
+
+dp_squares = find_closest_square()
+
+l = divide_grid(80, dp_squares)
+
+print(len(l))
 for each in l:
     print(each)
     print("================================")
